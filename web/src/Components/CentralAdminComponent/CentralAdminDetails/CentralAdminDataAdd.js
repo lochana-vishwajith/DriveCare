2b1108c7d317bfa2b52566c3e50942650e260a6f
Hasitha Bandara <bandarahasitha68gmail.com>
2021-08-20 09:50:34 +0530

    tempry commit1

--- web/src/Components/CentralAdminComponent/CentralAdminDetails/CentralAdminDataAdd.js
+++ web/src/Components/CentralAdminComponent/CentralAdminDetails/CentralAdminDataAdd.js
@@ -2,7 +2,7 @@ import { Grid, Paper, Link } from "@material-ui/core";
 import TextBox from "devextreme-react/text-box";
 import Validator, { RequiredRule } from "devextreme-react/validator";
 import React, { Component } from "react";
-
+import "./Centraladmin.css"
 import Button from "../../ButtonComponent/button";
 
 export default class PoliceStationLogin extends Component {
@@ -10,30 +10,62 @@ export default class PoliceStationLogin extends Component {
         super(props);
 
     }
+    //name,nicNumber,email,workstation,mobileNumber,officeAddress,officeNumber,officerRegistrationNumber
     render() {
         return (
             <div>
-                <div className="container">
-                    <div className="mt-1">
-                        <center>
-                            <div className="d-reg">
-                                <i>Welcome to</i>
-                            </div>
-                            <div className="d-dc">
-                                <b>Central Admin Data Add</b>
-                            </div>
-                        </center>
-                        <hr></hr>
-                    </div>
-                    <Grid>
-                        <Paper elevation={20}>
                             <div className="d-center-form">
+                                <form>
+                                    <div className="one-line-box">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="name" float = "right" >Name</label>
+                                            <input type="text" className="form-control" id="name"
+                                                   placeholder="Name" name="name"/>
+                                        </div>
+                                        <div className="text-box-2">
+                                            <label htmlFor="nicNumber">NIC Number</label>
+                                            <input type="text" className="form-control" id="nicNumber"
+                                                   placeholder="NIC Number" name="nicNumber"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="workstation">workstation</label>
+                                        <input type="text" className="form-control" id="workstation"
+                                               placeholder="workstation" name="workstation"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="OfficeAddress">officeAddress</label>
+                                        <input type="text" className="form-control" id="OfficeAddress"
+                                               placeholder="Office Address"  name ="OfficeAddress"/>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="email">Email</label>
+                                            <input type="text" className="form-control" id="email" name = "email" placeholder="Email"/>
+                                        </div>
+                                            <div className="form-group col-md-6">
+                                                <label htmlFor="officerRegistrationNumber">Officer RegistrationNumber</label>
+                                                <input type="text" className="form-control" id="officerRegistrationNumber" name = "officerRegistrationNumber"
+                                                       placeholder="Officer RegistrationNumber"/>
+                                            </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="officeTelephone" float = "right" >Office Telephone Number</label>
+                                            <input type="name" className="form-control" id="officeTelephone"
+                                                   placeholder="Office Telephone Number" name="officeTelephone"/>
+                                        </div>
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="mobileNumber">Mobile Number</label>
+                                            <input type="text" className="form-control" id="mobileNumber"
+                                                   placeholder="Mobile Number" name= " mobileNumber"/>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Sign in</button>
+                                </form>
 
                             </div>
-                        </Paper>
-                    </Grid>
-                </div>
-            </div>
+                       </div>
         );
     }
 }
--- web/src/Components/PoliceStation/AddPoliceStation/AddPoliceStation.js
+++ web/src/Components/PoliceStation/AddPoliceStation/AddPoliceStation.js
@@ -8,6 +8,15 @@ export default class PoliceStationLogin extends Component {
     constructor(props) {
         super(props);
 
+        this.state ={
+            registrationNo: '',
+            email:'',
+            workstation_Address:'',
+            mobile_Number:'',
+            office_Number:'',
+            password:'',
+            station_grade:''
+        }
     }
     render() {
         return (
@@ -27,6 +36,71 @@ export default class PoliceStationLogin extends Component {
                     <Grid>
                         <Paper elevation={20}>
                             <div className="d-center-form">
+
+
+                                <form>
+                                    <div className="one-line-box">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="name" float = "right" >registrationNo</label>
+                                            <input type="text" className="form-control" id="name"
+                                                   placeholder="Name" name="name"/>
+                                        </div>
+                                        <div className="text-box-2">
+                                            <label htmlFor="nicNumber">office_Number</label>
+                                            <input type="text" className="form-control" id="nicNumber"
+                                                   placeholder="NIC Number" name="nicNumber"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="workstation">workstation_Address</label>
+                                        <input type="text" className="form-control" id="workstation"
+                                               placeholder="workstation" name="workstation"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="OfficeAddress">email</label>
+                                        <input type="text" className="form-control" id="OfficeAddress"
+                                               placeholder="Office Address"  name ="OfficeAddress"/>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="email">Email</label>
+                                            <input type="text" className="form-control" id="email" name = "email" placeholder="Email"/>
+                                        </div>
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="officerRegistrationNumber">Officer RegistrationNumber</label>
+                                            <input type="text" className="form-control" id="officerRegistrationNumber" name = "officerRegistrationNumber"
+                                                   placeholder="Officer RegistrationNumber"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="officeTelephone" float = "right" >Office Telephone Number</label>
+                                            <input type="name" className="form-control" id="officeTelephone"
+                                                   placeholder="Office Telephone Number" name="officeTelephone"/>
+                                        </div>
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="mobileNumber">Mobile Number</label>
+                                            <input type="text" className="form-control" id="mobileNumber"
+                                                   placeholder="Mobile Number" name= " mobileNumber"/>
+                                        </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="password" password= "right" >Password</label>
+                                            <input type="password" className="form-control" id="password"
+                                                   placeholder="Password" name="password"/>
+                                        </div>
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="rPassword">Re-Enter Password</label>
+                                            <input type="password" className="form-control" id="rPassword"
+                                                   placeholder="Re-Enter Password" name= "rPassword"/>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" className="btn btn-primary">Sign in</button>
+                                </form>
+
+
                             </div>
                         </Paper>
                     </Grid>
