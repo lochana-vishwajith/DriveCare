2d06691084180b71c5de98e2f10560f01115ef5b
ushara <thimalushara@gmail.com>
2021-08-23 09:16:14 +0530

    backend court fixed the insert id issue

--- api/Routes/CourtPoliceRoutes.js
+++ api/Routes/CourtPoliceRoutes.js
@@ -13,33 +13,56 @@ router.post("/postc", async (req, res) => {
     comment,
   });
 
-  commentz
-    .save()
-    .then((result) => {
-      console.log(`Result eka - ${result}`);
+  const c = await commentz.save();
+  res.send(c);
 
-      Police.findByIdAndUpdate(officerID, {
-        $push: {
-          courtComments: result._id,
-        },
-      });
+  const dr = await Police.findByIdAndUpdate(c.officerID, {
+    $push: {
+      courtComments: c._id,
+    },
+  });
+  console.log(dr);
 
-      Judge.findByIdAndUpdate(judgeID, {
-        $push: {
-          officerComments: result._id,
-        },
-      })
+  const jd = await Judge.findByIdAndUpdate(c.judgeID, {
+    $push: {
+      officerComments: c._id,
+    },
+  });
 
-        .then((response) => {
-          res.send("Successfully added");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((error) => {
-      console.log(`Errorr - ${error}`);
-    });
+  console.log(jd);
+
+  // commentz
+  //   .save()
+  //   .then((result) => {
+  //     console.log(`Result eka - ${result}`);
+
+  //     Police.findByIdAndUpdate(officerID, {
+  //       $push: {
+  //         courtComments: result._id,
+  //       },
+  //     })
+
+  // Judge.findByIdAndUpdate(judgeID, {
+  //   $push: {
+  //     officerComments: result._id,
+  //   },
+  // })
+
+  //     .then((response) => {
+  //       res.send(`Response - ${response}`);
+  //       Judge.findByIdAndUpdate(judgeID, {
+  //         $push: {
+  //           officerComments: result._id,
+  //         },
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // })
+  // .catch((error) => {
+  //   console.log(`Errorr - ${error}`);
+  // });
 
   // const comment = new CourtPolice({
   //   date: req.body.date,
