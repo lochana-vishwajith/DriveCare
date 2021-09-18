95784efce22e97665d0a72f69b3cd71b7c52ec8d
Savith <savithdissanayake98@gmail.com>
2021-09-19 02:17:33 +0530

    pdf 40%

--- web/src/Components/DriverComponent/DriverSummaryComponent/DriverSummary.js
+++ web/src/Components/DriverComponent/DriverSummaryComponent/DriverSummary.js
@@ -3,12 +3,16 @@ import axios from "axios";
 import React, { Component } from "react";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 export default class DriverSummary extends Component {
   constructor(props) {
     super(props);
     this.state = {
       summaryDetails: [],
+      violation: [],
+      minor: [],
     };
   }
 
@@ -20,8 +24,68 @@ export default class DriverSummary extends Component {
       .then((res) => {
         console.log("data:", res.data);
         this.setState({ summaryDetails: res.data });
+        let tempViolation = [];
+        let tempMinor = [];
+        this.state.summaryDetails.map((item, index) => {
+          item.violationType.map((data, k) => {
+            tempMinor.push(item);
+            tempViolation.push(data);
+          });
+        });
+        this.setState({ violation: tempViolation });
+        this.setState({ minor: tempMinor });
+        console.log("VType:", this.state.violation);
+        console.log("MType:", this.state.minor);
       });
   }
+
+  generateSummary = () => {
+    // e.preventdefault();
+    const unit = "pt";
+    const size = "A4";
+    const orientation = "landscape";
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+
+    const title = "Driver Violation Summary";
+    const headers = [["Violation", "Fine Amount"]];
+    const head = [["Location", "Type", "Date"]];
+
+    const data = this.state.violation.map(
+      (item) => [
+        // item.violationType.map((detail) => [
+        item.description,
+        // item.place,
+        // item.fineType,
+        item.fineAmount,
+        // moment(item.offenceDate).format("DD-MM-YYYY"),
+        // ]),
+      ],
+      this.state.minor.map((item) => [
+        item.place,
+        item.fineType,
+        moment(item.offenceDate).format("DD-MM-YYYY"),
+      ])
+    );
+
+    // const data2 = this.state.minor.map((item) => [
+    //   item.place,
+    //   item.fineType,
+    //   moment(item.offenceDate).format("DD-MM-YYYY"),
+    // ]);
+
+    let contents = {
+      starty: 50,
+      head: headers,
+      body: data,
+    };
+
+    doc.setFontSize(20);
+    doc.text(title, 300, 60, null, null, "center");
+    require("jspdf-autotable");
+    doc.autoTable(contents);
+    doc.save("Driver_Violation_Summary.pdf");
+  };
   render() {
     return (
       <div>
@@ -37,6 +101,7 @@ export default class DriverSummary extends Component {
                 <button
                   type="button"
                   class="btn btn-outline-danger btn-sm px-4"
+                  onClick={this.generateSummary}
                 >
                   Download Report
                 </button>
